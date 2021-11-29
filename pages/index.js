f92e2c5a8250a33866ca4f65c07a1f7e14c9fc9b
rommomm <rommomm@i.ua>
2021-11-29 13:39:43 +0200

    crud functionality

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-function Page({ posts_ }) {
-  const [posts, setPosts] = useState(posts_);
+function App() {
+  const [posts, setPost] = useState([]);
   const [text, setText] = useState("");
+  const [postId, setPostId] = useState(null);
 
-  const deletepost = (id) => {
-    fetch(`http://localhost:8000/api/posts/${id}`, { method: "delete" }).then(
-      (res) => {
-        setPosts(posts.filter((p) => p.id !== id));
-      }
-    );
-  };
-
-  const requestOptions = () => {
-    fetch(`http://localhost:8000/api/posts`, {
-      method: "put",
-      headers: {
-        "Content-Type": "application/json",
-      },
-
-      body: JSON.stringify({ text }),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setText([...posts, res]);
-      });
-  };
-
-  const add = () => {
+  function saveData() {
     fetch(`http://localhost:8000/api/posts`, {
       method: "post",
       headers: {
@@ -38,37 +16,93 @@ function Page({ posts_ }) {
     })
       .then((res) => res.json())
       .then((res) => {
-        setPosts([...posts, res]);
+        setPost([...posts, res]);
+        
       });
   };
-  console.log("text", text);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+  function getPosts() {
+    fetch("http://localhost:8000/api/posts").then((result) => {
+      result.json().then((resp) => {
+        setPost(resp);
+        setText(resp[0].text);
+        setPostId(resp[0].id);
+      });
+    });
+  }
+
+  function deletePost(id) {
+    fetch(`http://localhost:8000/api/posts/${id}`, { method: "delete" }).then(
+      (res) => {
+        setPost(posts.filter((p) => p.id !== id));
+      }
+    );
+  };
+  function selectPost(id) {
+    const selectedPost = posts.find(post => post.id === id)
+    let item=selectedPost;
+    setText(item.text);
+    setPostId(item.id);
+  }
+  function updatePost() {
+    fetch(`http://localhost:8000/api/posts/${postId}`, {
+      method: "PUT",
+      headers:{
+        'Accept':'application/json',
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({text}),
+    }).then((result) => {
+      result.json().then((resp) => {
+        getPosts();
+      });
+    });
+  }
   return (
-    <>
+  <div class="block">
+<div class="tablePost">
+
+    <div>
+<h1 >ADD POST </h1>
       <input
         type="text"
+        value={text}
         placeholder="Text"
         onChange={(e) => setText(e.target.value)}
       />
-      <button onClick={() => add()}>Add</button>
+      <button onClick={() => saveData()}>Add</button>
+      <button onClick={updatePost}>Update Post</button>
+</div><br/>
+     <table border="1" style={{ float: "left" }}>
+        <tbody>
+          <tr>
+            <td>ID</td>
+            <td>Text</td>
+          </tr>
+          {posts.map((item, i) => (
+            <tr key={i}>
+              <td>{item.id}</td>
+              <td>{item.text}</td>
+              <td>
+                <button onClick={() => deletePost(item.id)}>Delete</button>
+              </td>
+              <td>
+                <button onClick={() => selectPost(item.id)}>Update</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+      </div>
 
-      {posts.map((post) => {
-        return (
-          <div key={post.id}>
-            <div>Post ID:{post.id}</div>
-            <div>Post text:{post.text}</div>
-            <button onClick={() => deletepost(post.id)}>DELETE</button>
-          </div>
-        );
-      })}
-    </>
+    
+   
   );
 }
 
-Page.getInitialProps = async (ctx) => {
-  const res = await fetch("http://localhost:8000/api/posts");
-  const json = await res.json();
-  console.log(json);
-  return { posts_: json };
-};
 
-export default Page;
+export default App;
