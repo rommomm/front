fd12800d8592564416b7569f9d989c8f030dee22
rommomm <92295078+rommomm@users.noreply.github.com>
2021-12-14 10:44:10 +0200

    fix

--- libs/apiClient.js
+++ libs/apiClient.js
@@ -2,8 +2,5 @@ import axios from "axios";
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 const apiClient = axios.create({
   baseURL: `${baseURL}`,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 export default apiClient;
