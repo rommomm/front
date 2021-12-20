40c6afb6ceab23cfea4a5879d9d5559a736d8065
rommomm <rommomm@i.ua>
2021-12-20 12:30:54 +0200

    valid

--- pages/login/index.js
+++ pages/login/index.js
@@ -90,14 +90,14 @@ const RegisterForm = () => {
                     <div className="col-md-12 mt-4">
                       <button
                         type="submit"
-                        className="w-full bg-blue-400 rounded-lg px-4 py-3 shadow-md"
+                        className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4  rounded"
                       >
                         Sign Up
                       </button>
                     </div>
                     <div className="text-grey-dark mt-6 text-center ">
-                      <Link href="/login">
-                        <a className="no-underline border-b border-blue text-blue">
+                      <Link href="/register">
+                        <a className="no-underline border-blue text-blue hover:underline">
                           Register
                         </a>
                       </Link>
